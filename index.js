ba59a94fcd2bafc71d0cc92e2b68bff98c79b3d2
psandesh64 <psandesh64@gmail.com>
2024-01-07 08:26:14 +0545

    Ex1 first commit

--- index.js
+++ index.js
@@ -29,21 +29,6 @@ app.use(requestLogger)
 app.get('/contact/',(request,response) => {
     response.json(contact)
 })
-app.get('/contact/:id',(request,response) => {
-    const id = Number(request.params.id)
-    const singleContact = contact.find(data => data.id === id)
-    if (singleContact) {response.json(singleContact)}
-    else (response.status(404).end())
-})
-app.delete('/contact/:id',(request,response) => {
-    const id = Number(request.params.id)
-    contact  = contact.filter(data => data.id !== id)
-    response.status(204).end()
-})
-app.post('/contact/',(request,response) => {
-    const newcontact = request.body
-    contact.push(newcontact)
-    response.json(contact)})
 
 const unknownEndpoint = (request,response) => {
     response.status(404).send({error:'unknown endpoint'})
